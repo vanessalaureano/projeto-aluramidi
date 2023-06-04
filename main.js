beb37bbb9df29aca724ab9bbff5ceed2df2026f3
vanessalaureano <solytagamer@gmail.com>
2023-06-04 02:17:55 -0300

    update

--- main.js
+++ main.js
@@ -10,6 +10,32 @@ function tocaSomClap () {
 
 document.querySelector('.tecla_clap').onclick = tocaSomClap; */
 
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla'); 
+
+let contador = 0;
+
+// condição enquanto estrutura de repetição
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom('#som_tecla');
+    };
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
 
