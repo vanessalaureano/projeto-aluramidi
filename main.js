ca9c3b54ba81028130881fd49d86302aa7d13369
vanessalaureano <solytagamer@gmail.com>
2023-06-04 02:22:06 -0300

    update

--- main.js
+++ main.js
@@ -24,18 +24,14 @@ while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
-
+    // template string
     const idAudio = `#som_${instrumento}`;
 
-    console.log(idAudio);
-
     tecla.onclick = function() {
-        tocaSom('#som_tecla');
+        tocaSom(idAudio);
     };
 
     contador = contador + 1;
-
-    console.log(contador);
 }
 
 
